c82f461b41021ecc2a79ec4354163e54659fde1f
Dylan <dylan.albert.osborne@gmail.com>
2019-11-18 16:17:38 +0000

    More stuff

---
+++ ex11.py
@@ -0,0 +1,15 @@
+import random
+def GuessGame(floor, ceil):
+	c_guess = random.randint(floor, ceil)
+	g_count = 0
+	is_correct = False
+	while not is_correct:
+		g_count += 1
+		p_guess = int(input(f" Please select a number between {floor} and {ceil}\n\n> "))
+		if p_guess == c_guess:
+			is_correct = True
+		else:
+			print(f"Guess {g_count} incorrect...")
+	print(f"Guess correct! Took {g_count} guesses.")
+
+GuessGame(1, 5)
